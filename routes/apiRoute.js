eea6404d770a50d4502ed88d6458a8b61de23b22
mich-hales <michellehales93@gmail.com>
2022-09-30 12:18:50 -0600

    update routes

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,10 +1,16 @@
+const fs = require('fs');
+
 // API Routes
-app.get('/api/notes', (req, res) => {
-    res.json({
-        // should read the db.json file and return all saved notes as JSON.
+const apiRoute = (app) => {
+    app.get('/api/notes', (req, res) => {
+        res.json({
+            // should read the db.json file and return all saved notes as JSON.
+        })
     })
-})
+    
+    app.post('/api/notes', (req, res) => {
+    
+    })
+};
 
-app.post('/api/notes', (req, res) => {
-
-})
+module.exports = apiRoute;
--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -1,9 +1,14 @@
+const path = require('path');
 
 // HTML Routes for notes.html and index.html
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
-});
+const htmlRoute = (app) => {
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '/../public/notes.html'));
+    });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-});
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '/../public/index.html'));
+    });
+}
+
+module.exports = htmlRoute;
