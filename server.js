3e8db6a642b5e32da93f2b4b6d5e7493124d61fc
mich-hales <michellehales93@gmail.com>
2022-10-02 10:30:04 -0600

    post request

--- server.js
+++ server.js
@@ -19,24 +19,43 @@ app.get('/notes', (req, res) => {
 // API Routes -- reads the db.json file and returns all saved notes as JSON
 app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (error) {
-            console.error(error);
+        if (err) {
+            console.error(err);
         };
         res.json(JSON.parse(data));
     });
 });
     
-    // random number
-
-    app.post('/api/notes', (req, res) => {
-        
-    })
+// random number
 
 
+// Receives a new note to save on the request body, adds to db.json file, and returns new note to client
+app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+    if (req.body) {
+        const newNote = {
+            id: uniqid(),
+            title,
+            text,
+        };
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const jsonParse = JSON.parse(data);
+                jsonParse.push(newNote);
+                fs.writeFile(newNote, json.stringify(jsonParse, null, 4), 
+                (err) => err ? console.error(err) : console.info(`nData written to ${newNote}`))
+            }
+        })
+    }
+})
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '/public/index.html'));
-    });
+
+// returns the index.html file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
 
 // Starts server -- listening
