5fbc85064938132f5f2554fe231f54f019b90c15
mich-hales <michellehales93@gmail.com>
2022-10-02 10:15:18 -0600

    updated readfile for api rooute:

--- server.js
+++ server.js
@@ -11,23 +11,28 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-// API Routes
-    app.get('/api/notes', (req, res) => {
-        res.json(data);
+// Route to notes.html file
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+// API Routes -- reads the db.json file and returns all saved notes as JSON
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (error) {
+            console.error(error);
+        };
+        res.json(JSON.parse(data));
     });
+});
     
-
     // random number
-
 
     app.post('/api/notes', (req, res) => {
         
     })
 
 
-    app.get('/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, '/public/notes.html'));
-    });
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '/public/index.html'));
